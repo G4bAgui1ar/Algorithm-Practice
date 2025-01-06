68e2f75ef41f9c3ca880d198ce2fdc53d54051fa
G4bAgui1ar <133580997+G4bAgui1ar@users.noreply.github.com>
2025-01-06 17:32:22 +0800

    commit

--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09151d7c811d313aa014b5091d5380ad3bcc3336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592d677ea0759559952d23a0da2518ecc2368a86")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
